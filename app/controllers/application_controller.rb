1c92a2f943f9517255865ad3c6931a97cc90981a
xfm04x <66931014+xfm04x@users.noreply.github.com>
2020-09-02 19:49:44 +0900

    ログイン新規登録分岐処理（application_controller user.rb shared/_header.html.erb 記述）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
   # basic認証private以下にメソッドとして定義↑呼び出し
-
   # 内容記述OK
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_day])
+  end
 
   private
 
