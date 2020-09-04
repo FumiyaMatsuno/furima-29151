395ecf77d181fd0e528fa387d944f5b80f451f3a
xfm04x <66931014+xfm04x@users.noreply.github.com>
2020-09-04 15:53:17 +0900

    ログイン新規登録出品するボタン　遷移実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :move_to_new, except: [:index,]
 
   def index
   end
@@ -6,10 +7,14 @@ class ItemsController < ApplicationController
   def new
   end
 
-  def edit
+  def show
   end
 
-  def update
+  private
+  def move_to_new
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 
 end
