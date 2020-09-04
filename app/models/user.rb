9e63d4e3d8764f3020285631ae3760bb56605b8c
xfm04x <66931014+xfm04x@users.noreply.github.com>
2020-09-05 00:53:08 +0900

    ログイン機能実装完了　カラム名変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,14 @@ class User < ApplicationRecord
 
 
   with_options presence: true do
-    validates :nickname, format: { with: /\A[a-z0-9]+\z/i, message: "は半角英数で入力してください。"}
-    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "は全角で入力してください。"}
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "は全角で入力してください。"}
-    validates :family_name_kana, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "は全角カタカナで入力して下さい。"}
-    validates :first_name_kana, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "は全角カタカナで入力して下さい。"}
-    validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "は半角英数で入力してください。"}
-    validates :encrypted_password, uniqueness: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "には英字と数字の両方を含めて設定してください。"}, length: { minimum: 6 }
+    validates :nickname
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :family_name_kana, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+    validates :first_name_kana, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
   end
+
+    validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}, length: { minimum: 6 }
+  
 end
