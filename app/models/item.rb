95511dd1b9bf6865d56e4fd9119032789088ccd8
xfm04x <66931014+xfm04x@users.noreply.github.com>
2020-09-08 18:04:34 +0900

    items.rbモデル　設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,14 @@
 class Item < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :description, presence: true, length: { maximum: 1000 }
+  validates :postage_id, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300 }
+  validates :category_id, presence: true
+  validates :sipping_day_id, presence: true
+  validates :condition_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :sold, inclusion: { in: [true, false] }
 
+  validates :user, presence: true
+  belongs_to :user
 end
