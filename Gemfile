39eb1f09a75dd5a558790704084bd44ab37f0b2a
xfm04x <66931014+xfm04x@users.noreply.github.com>
2020-09-09 18:33:34 +0900

    Activehash　ActiveStorage実装

--- Gemfile
+++ Gemfile
@@ -66,3 +66,7 @@ gem 'devise'
 gem 'active_hash'
 
 gem 'pry-rails'
+
+gem 'mini_magick'
+
+gem 'image_processing', '~> 1.2'
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :move_to_new, except: [:index]
 
   def index
+    @items = Item.order("created_at DESC")
   end
 
   def new
@@ -15,12 +16,24 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.save
+    @item = Item.new(item_params)
+
+    if @item.save
+      return redirect_to root_path
+    else
+      render "new"
+    end
   end
+
 
   private
 
   def move_to_new
     redirect_to new_user_session_path unless user_signed_in?
+  end
+
+
+  def item_params
+    params.require(:item).permit(:name, :description, :postage_id, :price, :category_id, :sipping_day_id, :condition_id, :prefecture_id, :image, :sold).merge(user_id: current_user.id)
   end
 end
